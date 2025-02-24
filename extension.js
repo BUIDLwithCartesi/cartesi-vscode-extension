674b87cdd1cb92181cd7b5b613f78e9bb1aadca8
surafeldev <surafel.nigusie.dev@gmail.com>
2025-02-24 14:39:59 +0300

    update readme file

--- extension.js
+++ extension.js
@@ -1,15 +1,51 @@
 import * as vscode from 'vscode';
 
 export function activate(context) {
-    console.log('cartesi VS code extention is now activated !');
+    console.log('Cartana is now activated!');
 
-    const createProjectCommand = vscode.commands.registerCommand('extension.createProject', () => {
+    const createProjectCommand = vscode.commands.registerCommand('cartana.create', () => {
         vscode.window.showInformationMessage('Create Project command executed!');
     });
+
+    const cartanaViewProvider = new CartanaViewProvider(context.extensionUri);
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(CartanaViewProvider.viewType, cartanaViewProvider)
+    );
 
     context.subscriptions.push(createProjectCommand);
 }
 
 export function deactivate() {
-    console.log('cartesi VS code extention is now deactivated !');
+    console.log('Cartana is now deactivated!');
+}
+
+class CartanaViewProvider {
+    static viewType = 'cartanaView';
+
+    constructor(private readonly extensionUri: vscode.Uri) {}
+
+    resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, _token: vscode.CancellationToken) {
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this.extensionUri]
+        };
+
+        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+    }
+
+    getHtmlForWebview(webview: vscode.Webview): string {
+        return `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Cartana</title>
+            </head>
+            <body>
+                <h1>Welcome to Cartana!</h1>
+            </body>
+            </html>
+        `;
+    }
 }
